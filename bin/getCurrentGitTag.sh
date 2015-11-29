8b50ee621a0f10d2e790b590858d1cdd4ec13170
Nicholas D. Socci <soccin@mskcc.org>
2015-11-29 14:56:14 -0500

    Fixed joined maf to have header with FACETS script and run info

--- bin/getCurrentGitTag.sh
+++ bin/getCurrentGitTag.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 REPO=$1
-git --git-dir=$REPO/.git --work-tree=$REPO describe --always --long
+BRANCH=$(git --git-dir=$REPO/.git --work-tree=$REPO describe --all)
+TAG=$(git --git-dir=$REPO/.git --work-tree=$REPO describe --long --always)
+ORIGIN=$(git --git-dir=$REPO/.git --work-tree=$REPO config --get remote.origin.url)
+UNTRACKED=$(git --git-dir=$REPO/.git --work-tree=$REPO status --porcelain)
+if [ "$UNTRACKED" != "" ]; then
+    UNTRACKED=" [UnCommited Changes $(date)]"
+fi
+echo "$ORIGIN ($BRANCH) ($TAG)$UNTRACKED"
--- joinFacetsMAF.sh
+++ joinFacetsMAF.sh
@@ -5,17 +5,27 @@ if [ "$#" != "1" ]; then
     exit
 fi
 
-FACETS_VERSION=$(~/bin/getCurrentGitTag.sh /home/socci/Code/Pipelines/FACETS/FACETS.app)
-FACETS_SUITE_VERSION=$(~/bin/getCurrentGitTag.sh /home/socci/Code/Pipelines/FACETS/FACETS.app/facets-suite)
+SDIR="$( cd "$( dirname "$0" )" && pwd )"
+
+FACETS_VERSION=$($SDIR/bin/getCurrentGitTag.sh $SDIR)
+FACETS_SUITE_VERSION=$($SDIR/bin/getCurrentGitTag.sh $SDIR/facets-suite)
 
 RUNDIR=$1
 cd $RUNDIR/facets
 PTAG=$(pwd | awk -F"/" '{print $7}')
 
-/home/socci/Code/Pipelines/FACETS/FACETS.app/doJoinMaf.py
+#$SDIR/doJoinMaf.py
 ORIGMAF=$(ls ../post/*___SOMATIC.vep.maf)
 FACETMAF=${ORIGMAF/___SOMATIC.vep.maf/___SOMATIC_FACETS.vep.maf}
 
+egrep "^#" $ORIGMAF >$FACETMAF
+echo "#CBE:$FACETS_VERSION" >>$FACETMAF
+echo "#CBE:SubMod:$FACETS_SUITE_VERSION" >>$FACETMAF
+echo "#CBE:$0 $*" >>$FACETMAF
+$SDIR/bin/getFacetsRunParameters.py $(ls facets/*/*hisens.out | head -1) >>$FACETMAF
+egrep -v "^#" $ORIGMAF >>$FACETMAF
+less $FACETMAF
+exit
 
 ls facets/*/*hisens.seg | head -1 | xargs head -1 >${PTAG}____hisens.seg
 ls facets/*/*hisens.seg | xargs egrep -hv chrom >>${PTAG}____hisens.seg
