40ac8b7fb7a984743619353944808e3fc3a05e36
Nicholas D. Socci <soccin@mskcc.org>
2016-01-11 18:13:14 -0500

    Fixed how project dirs are found (search CMO folders)
    Now search both projects/BIC and projects/CMO to find project
    directory; also check for correct number of args

--- bProcess.sh
+++ bProcess.sh
@@ -9,10 +9,23 @@ SDIR="$( cd "$( dirname "$0" )" && pwd )"
 
 PIPELINEDIR=$1
 projectNo=$(echo $PIPELINEDIR | perl -ne 'm|/Proj_([^/\s]*)|; print $1')
-PROJECTDIR=$(find /ifs/projects/BIC -type d | fgrep -v drafts | egrep "Proj_$projectNo$")
+
+NUMDIRS=$(find /ifs/projects/BIC /ifs/projects/CMO -type d | egrep -v "(drafts|archive)" | egrep "Proj_$projectNo$" | wc -l)
+PROJECTDIR=$(find /ifs/projects/BIC /ifs/projects/CMO -type d | egrep -v "(drafts|archive)" | egrep "Proj_$projectNo$")
+
+SCRIPT=$(basename $0)
+if [ "$NUMDIRS" != "1" ]; then
+    echo $SCRIPT :: Problem finding project files for Proj_$projectNo
+    echo $SCRIPT NUMDIRS=$NUMDIRS
+    echo
+    exit
+fi
+
+echo $SCRIPT PROJECTDIR=\"$PROJECTDIR\"
+
 BAMDIR=$PIPELINEDIR/alignments
 
-echo
+echo $SCRIPT
 echo PROJECTDIR=$PROJECTDIR
 echo PIPELINEDIR=$PIPELINEDIR
 echo pairingFile=$PROJECTDIR/*_sample_pairing.txt
