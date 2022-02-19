40967360d0271630f9488f5d5828def19c1ad8c5
Nicholas D. Socci <soccin@mskcc.org>
2022-02-19 16:59:44 -0500

    cleanup and start work on new run.sh

--- run.sh
+++ run.sh
@@ -2,52 +2,24 @@
 
 SDIR="$( cd "$( dirname "$0" )" && pwd )"
 
-module load python/2.7.14
-module load singularity/3.3.0
+usage() {
 
-if [ "$#" -lt "2" ]; then
-	echo
-	echo "Too few arguments"
-	echo
-	echo "    usage:: FACETS.app/run.sh NORMALBAM TUMORBAM [TAG]"
-	echo
-	exit
-fi
+    echo
+    echo "    usage:: FACETS.app/run.sh TUMORBAM NORMALBAM"
+    echo
+    exit
 
-NORMALBAM=$1; shift
-TUMORBAM=$1; shift
+}
 
-NBASE=$(basename $NORMALBAM | sed 's/.bam//')
-TBASE=$(basename $TUMORBAM | sed 's/.bam//')
 
-if [ "$#" -lt "1" ]; then
-    TAG=${TBASE}____${NBASE}
-else
-    TAG=$1; shift
-fi
-FTAG=$($SDIR/bin/prettyTags.py $TAG)
 
-uuid=$(echo $FTAG | md5sum - | awk '{print $1}')
-D1=$(echo $uuid | perl -ne 'm/^(..)/;print $1')
+# NORMALBAM=$1; shift
+# TUMORBAM=$1; shift
 
-ODIR=results/$D1/$FTAG
+# NBASE=$(basename $NORMALBAM | sed 's/.bam//')
+# TBASE=$(basename $TUMORBAM | sed 's/.bam//')
 
-echo ODIR=$ODIR
+# mkdir -p $ODIR
 
-mkdir -p $ODIR
 
-$SDIR/bin/fillTemplate \
-    $SDIR/TEMPLATE.yaml \
-    NORMAL=$NORMALBAM \
-    TUMOR=$TUMORBAM \
-    PREFIX=$FTAG \
-    > $ODIR/input.yaml
-
-. $SDIR/venv/bin/activate
-
-cwltool \
-    --outdir $ODIR \
-    --singularity $SDIR/cmoflow_facets_cwl-1.0.0/cmoflow_facets.cwl \
-    $ODIR/input.yaml \
-    > $ODIR/log 2>&1
 
