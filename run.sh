9033cff63aea023175124e4b7a9d1b8a65459aba
Nicholas D. Socci <soccin@mskcc.org>
2022-02-19 17:14:31 -0500

    Minimal run.sh

--- run.sh
+++ run.sh
@@ -5,16 +5,35 @@ SDIR="$( cd "$( dirname "$0" )" && pwd )"
 usage() {
 
     echo
-    echo "    usage:: FACETS.app/run.sh TUMORBAM NORMALBAM"
+    echo "    usage:: FACETS.app/run.sh [--tag TAG] TUMORBAM [NORMALBAM]"
     echo
     exit
 
 }
 
+while [[ $1 =~ ^- ]]; do
+  case $1 in
+    --tag)
+      TAG="$2"
+      shift 2
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      usage
+      ;;
+  esac
+done
 
+if [[ "$#" -lt 1 ]]; then
+    usage
+fi
 
-# NORMALBAM=$1; shift
-# TUMORBAM=$1; shift
+TUMORBAM=$1; shift
+NORMALBAM=$1; shift
+
+echo "TAG=\"$TAG\""
+echo "TUMORBAM="$TUMORBAM
+echo "NORMALBAM=\"$NORMALBAM\""
 
 # NBASE=$(basename $NORMALBAM | sed 's/.bam//')
 # TBASE=$(basename $TUMORBAM | sed 's/.bam//')
